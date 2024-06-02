528e0388d5f0065ae7da0fe748ccf3b83778423c
Rafael-dev2 <rafasantosalves2013@gmail.com>
2024-06-02 13:11:58 -0300

    MVP funcional

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
 canvas = document.querySelector("#Sketch");
-for(i = 0;i < 5;i++){
-    pixel = document.createElement("div");
-    pixel.setAttribute("style","background-color: blue");
-    pixel.setAttribute("style","border-radius: 3px");
-    pixel.textContent = "ola";
-    canvas.appendChild(pixel);
+createbtn = document.querySelector("#topbtn");
+deletebtn = document.querySelector("#deletebtn");
+let input;
+createbtn.addEventListener("click",() => {
+    input = prompt("How many squares?");
+    if(input > 100){
+        input = 100;
+    }
+    generateCanvas(input);
+})
+deletebtn.addEventListener("click",() =>{
+    document.querySelector("#subcanvas").remove();
+})
+
+function generateCanvas(input){
+subcanvas = document.createElement("div");
+subcanvas.setAttribute("id","subcanvas");
+
+size = 960/input;
+str = size.toString();
+for(i = 0;i < input;i++){
+    let line = document.createElement("div");
+    line.setAttribute("id","line")
+    line.setAttribute("style","font-size: 0px;")
+    for(j = 0;j < input;j++){
+        let pixel = document.createElement("div");
+        pixel.setAttribute("style","background-color: white;" + "width:"+str+"px;"+
+            "height: "+str+"px;"+ 
+            'display: inline-flex;'+
+            'align-self: center;'
+                     );
+        pixel.setAttribute("id","pixel");
+        line.appendChild(pixel);
+        pixel.addEventListener("mouseover",() => {
+        pixel.setAttribute("style","background-color: red;"+"width:"+str+"px;"+
+            "height: "+str+"px;"+
+            'display: inline-flex;'+
+            'align-self: center;'+ 
+            'text-color:red;'
+     );
+    })
+}
+    subcanvas.appendChild(line);
+    canvas.appendChild(subcanvas);
+}
 }
